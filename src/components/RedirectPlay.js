6995ac4e0de70d2130bb5ef7511e41456545930d
Castolo-10 <leonardo.castolo@alumnos.udg.mx>
2020-11-18 11:27:01 -0600

    ajustes deploy test

--- src/components/RedirectPlay.js
+++ src/components/RedirectPlay.js
@@ -1,3 +1,4 @@
 export default function RedirectPlay(){
-    window.location = 'http://localhost:5500/Documentos/Version%201_Web/Version%201_Web/index.html'
+    //window.location = 'http://localhost:5500/Documentos/Version%201_Web/Version%201_Web/index.html'
+    window.location = 'https://savingtheword-game.herokuapp.com/index.html'
 }
